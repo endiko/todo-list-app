6912844a5ea4c4f4df8e7a78a7498b450c59762b
endiko <kogay.nadezhda@gmail.com>
2019-12-25 12:53:38 +0500

    Add main structure to project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
+import Todos from './components/Todos/Todos';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Nav from './components/Nav/Nav';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: []
+    };
+  }
+
+  componentDidMount() {
+    console.log('component Did Mount');
+    fetch('https://jsonplaceholder.typicode.com/todos?userId=1')
+      .then(response => {
+        let data = response.json();
+        return data;
+      })
+      .then(
+        result => {
+          this.setState({
+            todos: result
+          });
+        },
+        error => {
+          this.setState({
+            error
+          });
+        }
+      );
+  }
+
+  editTodoHandler = (e, index) => {
+    let copyTodo = [...this.state.todos];
+    let todo = copyTodo[index];
+    todo.title = e.target.value;
+    copyTodo[index] = todo;
+    this.setState({
+      todos: copyTodo
+    });
+  };
+  deleteTodoHandler = index => {
+    let copyTodo = [...this.state.todos];
+    copyTodo.splice(index, 1);
+    this.setState({
+      todos: copyTodo
+    });
+  };
+
+  render() {
+    console.log('Render');
+    const todos = this.state.todos;
+    return (
+      <div className='container'>
+        <h1>ToDo List</h1>
+        <form></form>
+        <Todos
+          todos={todos}
+          onEditTodo={this.editTodoHandler}
+          onDeleteTodo={this.deleteTodoHandler}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
