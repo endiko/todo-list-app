9326d5590a4b8b5bdad63247102e67ed879e7b90
Nadezhda Kogay <kogay.nadezhda@gmail.com>
2019-12-26 12:45:36 +0500

    Add a new todo item component and functionality

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import About from './pages/About/About';
 import Nav from './components/Nav/Nav';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
+const textPlaceholder = 'New todo ...';
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      todos: []
+      todos: [],
+      newText: textPlaceholder
     };
   }
 
@@ -53,17 +56,67 @@ class App extends Component {
     });
   };
 
+  completeTodoHandler = (e, index) => {
+    let todo = this.state.todos[index];
+    todo.completed = e.target.checked;
+    let copyTodos = [...this.state.todos];
+    copyTodos[index] = todo;
+    this.setState({
+      todos: copyTodos
+    })
+  }
+
+  inputFocusHandler = () => {
+    this.setState({
+      newText: ''
+    })
+  }
+
+  inputChangeHandler = (e) => {
+    this.setState({
+      newText: e.target.value
+    })
+  }
+
+  onEnterKeyHandler = (e) => {
+    console.log(e.key)
+    if (e.key === 'Enter') {
+      let newTodo = {};
+      let len = this.state.todos.length;
+      newTodo.userId = this.state.todos[0].userId;
+      newTodo.id = len + 1;
+      newTodo.title = e.target.value;
+      newTodo.completed = false;
+      let copyTodos = [...this.state.todos];
+      copyTodos.push(newTodo);
+      this.setState({
+        todos: copyTodos,
+        newText: ''
+      })
+    }
+  }
+
+  inputBlurHandler = () => {
+    this.setState({
+      newText: textPlaceholder
+    })
+  }
   render() {
     console.log('Render');
     const todos = this.state.todos;
     return (
       <div className='container'>
-        <h1>ToDo List</h1>
-        <form></form>
+        <h1 className="main-title">ToDo List</h1>
+        <div className="new-todo__container">
+          <label htmlFor="newTodo">
+            <input type="text" name="newTodo" className="new-todo__text" placeholder={this.state.newText} value={this.state.newText} onClick={this.inputFocusHandler} onBlur={this.inputBlurHandler} onChange={this.inputChangeHandler} onKeyPress={this.onEnterKeyHandler} onke />
+          </label>
+        </div>
         <Todos
           todos={todos}
           onEditTodo={this.editTodoHandler}
           onDeleteTodo={this.deleteTodoHandler}
+          onCompleteTodo={this.completeTodoHandler}
         />
       </div>
     );
