9e7c4d5364716528afa0a82d883bc2f3900a329f
Nadezhda Kogay <kogay.nadezhda@gmail.com>
2019-12-26 12:47:36 +0500

    Add event handlers. Change the component structure

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import './Todo.scss';
 
-export default props => (
-  <li className='todo-item'>
-    <label className='label' htmlFor="checkbox">
-      <input type='checkbox' name="checkbox" className='todo-checkbox' />
-    </label>
-    <label className='label' htmlFor="todoText">
-      <input
-        type='text'
-        name="todoText"
-        className='todo-text'
-        placeholder={props.title}
-        value={props.title}
-        onChange={props.onEditTodo}
-      />
-    </label>
+export default props => {
+  let labelForStyle = `todoText-${props.todo.id}`;
 
-    <button className='btn btn--delete' onClick={props.onDeleteTodo}>
-      <span className='btn-text btn-text--delete'>&times;</span>
-    </button>
-  </li>
-);
+  return (
+    <li className='todo-item'>
+      <input type='checkbox' name="checkbox" id={labelForStyle} aria-label='original checkbox' checked={props.todo.completed}
+        onChange={props.onCompleteTodo} />
+      <label className='label' htmlFor={labelForStyle}>
+        <span className="todo-checkbox" aria-label='custom checkbox'></span>
+        <input
+          type='text'
+          name="todoText"
+          className='todo-text'
+          placeholder={props.todo.title}
+          aria-label='to do single item'
+          value={props.todo.title}
+          onChange={props.onEditTodo}
+          readOnly={props.todo.completed}
+        />
+      </label>
+
+      <button className='btn btn--delete' aria-label='delete button' onClick={props.onDeleteTodo}>
+        <span className='btn-text btn-text--delete'>&times;</span>
+      </button>
+    </li>
+  )
+};
